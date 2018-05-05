1d67875b946f9f4a101b057a4f5444afc4401fc9
Parker Dinkins <parkerdinkins@gmail.com>
2018-05-05 08:07:31 -0400

    0.0.02
    network updates for testing

--- pynetwork/serverclient.py
+++ pynetwork/serverclient.py
@@ -3,11 +3,11 @@
 import socket
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-soc.connect(("192.168.1.17", 12345))
+soc.connect(("", 1234))
 
-clients_input = input("What you want to proceed my dear client?\n")  
+clients_input = input("input> \n")  
 soc.send(clients_input.encode("utf8")) # we must encode the string to bytes  
 result_bytes = soc.recv(4096) # the number means how the response can be in bytes  
 result_string = result_bytes.decode("utf8") # the return will be in bytes, so decode
 
-print("Result from server is {}".format(result_string))  
+print("data for server> {}".format(result_string))  
--- pynetwork/tpls_server.py
+++ pynetwork/tpls_server.py
@@ -1,13 +1,14 @@
 # server.py
 import logging
-
 logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s', )
 logger = logging.getLogger(__name__)
 
 
-def do_some_stuffs_with_input(input_string):  
-    print("Processing...")
-    autolog('do')
+local_ip = "192.168.1.5"
+n_port = 1234
+
+def catch_input(input_string):  
+    autolog()
     return input_string[::-1]
 
 def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
@@ -24,13 +25,13 @@ def client_thread(conn, ip, port, MAX_BUFFER_SIZE = 4096):
     # decode input and strip the end of line
     input_from_client = input_from_client_bytes.decode("utf8").rstrip()
 
-    res = do_some_stuffs_with_input(input_from_client)
+    res = catch_input(input_from_client)
     print("Result of processing {} is: {}".format(input_from_client, res))
 
     vysl = res.encode("utf8")  # encode the result string
     conn.sendall(vysl)  # send it to client
     conn.close()  # close connection
-    autolog('client thread')
+    autolog()
     print('Connection ' + ip + ':' + port + " ended")
 
 def start_server():
@@ -38,11 +39,11 @@ def start_server():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this is for easy starting/killing the app
     soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    autolog('start server')
+    autolog()
     print('Socket created')
 
     try:
-        soc.bind(("192.168.1.5", 80))
+        soc.bind((local_ip, n_port))
         print('Socket bind complete')
     except socket.error as msg:
         import sys
@@ -72,14 +73,13 @@ def start_server():
     
     soc.close()
 
-def autolog(message):
+def autolog():
     import inspect, logging
     # Get the previous frame in the stack, otherwise it would
     # be this function!!!
     func = inspect.currentframe().f_back.f_code
     # Dump the message + the name of this function to the log.
-    logging.debug("%s: %s in %s:%i" % (
-        message, 
+    logging.debug("%s in %s:%i" % (
         func.co_name, 
         func.co_filename, 
         func.co_firstlineno
